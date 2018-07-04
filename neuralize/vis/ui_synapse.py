a3cfe1534772f39719979026830dfb575e103996
Brandon Wang <sourbrandon@gmail.com>
2018-07-04 11:50:10 -0500

    added pyqt visualizations

--- neuralize/vis/ui_synapse.py
+++ neuralize/vis/ui_synapse.py
@@ -6,11 +6,8 @@ class Synapse:
         self.from_neuron = from_neuron
         self.to_neuron = to_neuron
         self.weight = weight
-        half_rgb = [i / 2 for i in color.getRgb()[:3]]
-        weighted_half_rgb = [i * weight for i in half_rgb]
-        weighted_rgb = [value + half_rgb[i] for i, value in enumerate(weighted_half_rgb)]
-        print(weight, weighted_rgb)
-        self.color = QColor(*weighted_rgb, 255)
+        self.base_color = color
+        self.set_color(weight)
 
     def draw(self, painter):
         pen = QPen(self.color)
@@ -21,3 +18,10 @@ class Synapse:
     def update_weight(self, new_weight):
         self.weight = new_weight
         self.color = self.color
+
+    def set_color(self, weight):
+        half_rgb = [i / 2 for i in self.base_color.getRgb()[:3]]
+        weighted_half_rgb = [i * weight for i in half_rgb]
+        weighted_rgb = [value + half_rgb[i] for i, value in enumerate(weighted_half_rgb)]
+        print(weight, weighted_rgb)
+        self.self.base_color = QColor(*weighted_rgb, 255)
